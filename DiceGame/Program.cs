6174ce7a617e84f78136cff51b20d76abbf28114
jmdaugherty94@gmail.com <jmdaugherty94@gmail.com>
2021-05-24 10:44:11 -0400

    Add project files.

---
+++ DiceGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+
+            int roll1 = dice.Next(1, 7);
+            int roll2 = dice.Next(1, 7);
+            int roll3 = dice.Next(1, 7);
+            int total = roll1 + roll2 + roll3;
+
+            Console.WriteLine($"Roll Total: {roll1} + {roll2} + {roll3} = {total}");
+
+            if(total < 14)
+            {
+                Console.WriteLine("LOSER");
+            }
+            if(total >= 14)
+            {
+                Console.WriteLine("WINNER");
+            }
+        }
+    }
+}
